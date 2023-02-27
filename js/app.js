32fd7f41b7db45d6693e7d0dffaeee339bd8c6c3
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-27 15:57:10 +0100

    FEAT base exercise done , working on bonus

--- js/app.js
+++ js/app.js
@@ -3,29 +3,41 @@ const { createApp } = Vue
 
 //prendo url con axios e la sua proprieta' get
 
-axios
-.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((res)=>{
-  console.log(res.data.response) //controllo nella console rete, e nello specifico su mail per vedere se la mail viene stampata ogni volta.
-})
 
 
 //stampare nell'html 10 mail randomiche creando un ciclo
+//queste mail verranno pushate nell'array email vuoto dentro data, e dopo il push verranno stampate.
 
-  createApp({
-
+createApp({
+  
     data() {
       return {
-        email: '',
+        email: [],
       }
     },
-
-
+    
+    
     methods:{
+      
+      generateMailFromApi(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res)=>{
+          console.log(res.data.response) //controllo nella console rete, e nello specifico su mail per vedere se la mail viene stampata ogni volta.
+          //da qui in poi dovro' pushare su this.email[] il contenuto prodotto dalla FAKE API.
+          this.email.push(res.data.response)
+        })
+      },
 
+      
 
 
     },
 
+    mounted() {
+      for(let i = 0; i < 10; i++){
+        this.generateMailFromApi()
+      }
+    }
 
   }).mount('#app')
