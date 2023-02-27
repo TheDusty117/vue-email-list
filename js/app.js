f3829a14d1a84ad252f65a37a998ea619d84b96a
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-27 16:53:25 +0100

    FEAT alternative way to generate mails

--- js/app.js
+++ js/app.js
@@ -20,24 +20,25 @@ createApp({
     methods:{
       
       generateMailFromApi(){
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res)=>{
-          console.log(res.data.response) //controllo nella console rete, e nello specifico su mail per vedere se la mail viene stampata ogni volta.
-          //da qui in poi dovro' pushare su this.email[] il contenuto prodotto dalla FAKE API.
-          this.email.push(res.data.response)
-        })
-      },
+        for(let i=0; i<10; i++){
+          axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((res)=>{
+            console.log(res.data.response) //controllo nella console rete, e nello specifico su mail per vedere se la mail viene stampata ogni volta.
+            //da qui in poi dovro' pushare su this.email[] il contenuto prodotto dalla FAKE API.
+            this.email.push(res.data.response)
+          })
+        }
 
-      
+      }
 
 
     },
 
-    mounted() {
-      for(let i = 0; i < 10; i++){
-        this.generateMailFromApi()
-      }
-    }
+    // mounted() {
+    //   for(let i = 0; i < 10; i++){
+    //     this.generateMailFromApi()
+    //   }
+    // }
 
   }).mount('#app')
